3376330050b655406107beab8c9b1225aa3b2eec
RaWa <1>
2014-09-26 01:30:38 +0200

    HashBeat.c

---
+++ HashBeat.c
@@ -0,0 +1,16 @@
+/* localtime example */
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+  time_t rawtime;
+  struct tm * timeinfo;
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  printf ( "Current local time and date: %s", asctime (timeinfo) );
+  printf ( "Current local time and date: %ld", rawtime );
+
+  return 0;
+}
