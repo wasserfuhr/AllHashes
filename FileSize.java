f9234c40f538fcc5d490711f16eb467013f79030
RaWa <1>
2016-01-08 01:41:17 +0100

    ~FileSize.java

--- FileSize.java
+++ FileSize.java
@@ -1,16 +1,18 @@
 import java.io.*;
 
 public class FileSize {
- public static String ext(String hash) {
-  LineNumberReader lnr = new LineNumberReader(new InputStreamReader(System.in));
-  return "txt";
- }
 
  public static String SD() throws Exception {
   if (System.getProperty("user.home").equals("/Users/rawa")) {
    return "/Volumes/SPACEDRIVE1/SpaceDrive/";
   }
   return "/home/rawa/SpaceDrive/";
+ }
+
+ public static String ext(String hash) throws Exception {
+  String s059a="059af36cfe1132802ed9475f0a42d93033867ed843512e76a211491a17b1803f";
+  LineNumberReader lnr = new LineNumberReader(new InputStreamReader(new FileInputStream(SD()+s059a+".txt")));
+  return "txt";
  }
 
  public static void main(String[] args) throws Exception {
