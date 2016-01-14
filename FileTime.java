1e3de4948026dc3b445831c4fd4f482baa8a67ce
RaWa <rawa@borg.local>
2016-01-14 20:51:39 +0100

    +FileTime.java

---
+++ FileTime.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ByteSig {
+ public static String SD() throws Exception {
+  if (System.getProperty("user.home").equals("/Users/rawa")) {
+   return "/Volumes/SPACEDRIVE1/SpaceDrive/";
+  }
+  return "/home/rawa/SpaceDrive/";
+ }
+
+ public static void main(String[] args) throws Exception {
+  File dir=new File(SD());
+  dir =new File("/Users/rawa/Public/AllHashes");
+
+  File[] files = dir.listFiles();
+  for (File f : files) {
+      if(f.getName().length()>4){
+   FileInputStream fr=new FileInputStream(f);
+   boolean[] sig=new boolean[256];
+   int b;
+   while((b=fr.read())>=0) {
+    sig[b]=true;
+   }
+   //   System.out.print(f.getName().substring(0,64)+" ");
+   System.out.print(f.getName()+" ");
+   for(int i=255; i>=0; i-=4) {
+    System.out.print(
+     "0123456789abcdef".charAt(
+       (sig[i]?8:0)+
+       (sig[i-1]?4:0)+
+       (sig[i-2]?2:0)+
+       (sig[i-3]?1:0)));
+   }
+      }   System.out.println();
+  }
+ }
+}
