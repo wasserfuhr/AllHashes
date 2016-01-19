2103def6742603a59d9ef661bf7a71a2f2c8d525
RaWa <1>
2016-01-19 06:05:13 +0100

    +SpaceDrive.java

---
+++ SpaceDrive.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class SpaceDrive {
+ public static void dir(String d) throws Exception {
+  for (File f:new File(d).listFiles()) {
+   if (!f.isHidden()){
+    String s=f.getParent()+"/"+f.getName();
+    if (f.isDirectory()) {
+     dir(s);
+    } else {
+     System.out.println(s);
+    }
+   }
+  }
+ }
+ public static void main(String[] args) throws Exception {
+  dir(args[0]);
+ }
+}
