77c582c2517bc2850d5a1f67483f0215d97e6dbc
RaWa <1>
2016-01-19 06:21:03 +0100

    mod SpaceDrive.java

--- SpaceDrive.java
+++ SpaceDrive.java
@@ -1,16 +1,19 @@
 import java.io.File;
+import java.util.*;
 
 public class SpaceDrive {
  public static void dir(String d) throws Exception {
+  Hashtable h=new Hashtable();
   for (File f:new File(d).listFiles()) {
-   if (!f.isHidden()){
-    String s=f.getParent()+"/"+f.getName();
-    if (f.isDirectory()) {
-     dir(s);
-    } else {
-     System.out.println(s);
-    }
+   String ext=f.getName().substring(65);
+   if (h.containsKey(ext)) {
+       h.put(ext,(Integer)h.get(ext)+1);
+   } else {
+    h.put(ext,1);
    }
+  }
+  for (Object k:h.keySet()) {
+   System.out.println(k+" "+h.get(k));
   }
  }
  public static void main(String[] args) throws Exception {
