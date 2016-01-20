1cb2112095b1e2b46b37e98f8c0b7ed5318f8e6e
RaWa <1>
2016-01-20 19:08:51 +0100

    ~Ls.java

--- Ls.java
+++ Ls.java
@@ -1,17 +1,17 @@
 import java.io.*;
 
 public class Ls {
-//059af36cfe1132802ed9475f0a42d93033867ed843512e76a211491a17b1803f.txt
  public static void main(String[] args) throws Exception {
-
-LineNumberReader lr=new LineNumberReader(new FileReader(new File("H:/SpaceDrive/059af36cfe1132802ed9475f0a42d93033867ed843512e76a211491a17b1803f.txt")));
-String l;int ct=0;
-while(null!=(l=lr.readLine())) {
+  LineNumberReader lr=new LineNumberReader(new FileReader(
+   new File(args[0]+
+    "059af36cfe1132802ed9475f0a42d93033867ed843512e76a211491a17b1803f.txt")));
+  String l;
+  long ct=0;
+  while(null!=(l=lr.readLine())) {
    System.out.println(ct+" "+l);
-ct++;
-}
-
-  File d=new File("H:/SpaceDrive");
+   ct++;
+  }
+  File d=new File(args[0]);
   String fs[]=d.list();
   for(String s:fs) {
    System.out.println(s);
