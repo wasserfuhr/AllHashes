0b6dbcab8508ad30d887ceef20c31446035862f8
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-01-21 11:04:11 +0100

    ~Eye.java

--- Eye.java
+++ Eye.java
@@ -6,9 +6,12 @@ public class Eye {
  public static void main(String[] args) throws Exception {
   File f = new File(args[0]+"ffe73037035bd0ba401dab87e3d1bb422c3590107aa70e9a14cb2ac77d110ae9.jpg");
   //http://stackoverflow.com/questions/6524196/java-get-pixel-array-from-image
-  BufferedImage bi=ImageIO.read(f);
-  for (int x=0;x<16;x++) {
-      //System.out.println(d.getName()+"-"+t.getTagTypeHex()+" "+t);
+  BufferedImage i=ImageIO.read(f);
+  for (int y=0;y<16;y++) {
+   for (int x=0;x<16;x++) {
+    System.out.print(i.getRGB(0,x));    
+   }
+   System.out.println();
   }
  }
 }
