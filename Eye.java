afbae02bbd3c3ab7f86fb68e90c278bfc6c67ac9
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-01-21 11:22:55 +0100

    ~Eye.java -seeing the NooSphere

--- Eye.java
+++ Eye.java
@@ -7,10 +7,12 @@ public class Eye {
   File f = new File(args[0]+"ffe73037035bd0ba401dab87e3d1bb422c3590107aa70e9a14cb2ac77d110ae9.jpg");
   //http://stackoverflow.com/questions/6524196/java-get-pixel-array-from-image
   BufferedImage i=ImageIO.read(f);
-  for (int y=0;y<16;y++) {
-   for (int x=0;x<16;x++) {
-    int p=i.getRGB(y,x);
-    System.out.print(String.format("%02x ",p));
+  System.out.println(i.getWidth()+"x"+i.getHeight());
+  for (int y=0;y<64;y++) {
+   for (int x=0;x<64;x++) {
+    int p=i.getRGB(440+x,1520+y);
+    String s=String.format("%02x ",p);
+    System.out.print(s.substring(2,3));
    }
    System.out.println();
   }
