418527be09f837c40a2ea40afaa6503cb774d2b3
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-01-22 16:20:32 +0100

    ~Ear.java

--- Ear.java
+++ Ear.java
@@ -1,8 +1,22 @@
 import java.io.*;
 
 public class Ear {
- public static void main(String[] args) throws Exception {
-  FileInputStream fi = new FileInputStream("/Users/rawa/Desktop/raw.wav");
-  System.out.println(fi.read());
+ public static void main(String[] a) throws Exception {
+  //http://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
+     //  FileInputStream fi = new FileInputStream("/Users/rawa/Desktop/raw.wav");
+     //  FileInputStream fi = new FileInputStream("/Users/rawa/Desktop/Schoen.wav");
+  FileInputStream fi = new FileInputStream(a[0]);
+  int b;
+  while((b=fi.read())>=0) {
+   if(b>127)b=255-b;
+   b=b/4;
+   for (int i=0;i<64+b;i++) {
+    System.out.print(".");
+   }
+   fi.read();
+   fi.read();
+   fi.read();
+   System.out.println();
+  }
  }
 }
