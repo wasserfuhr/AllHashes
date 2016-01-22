e29e1f033c377f7613cb4310059fc47e7af3bee4
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-01-22 16:51:09 +0100

    ~Ear.java

--- Ear.java
+++ Ear.java
@@ -5,12 +5,13 @@ public class Ear {
   //http://stackoverflow.com/questions/8754111/how-to-read-the-data-in-a-wav-file-to-an-array
      //  FileInputStream fi = new FileInputStream("/Users/rawa/Desktop/raw.wav");
      //  FileInputStream fi = new FileInputStream("/Users/rawa/Desktop/Schoen.wav");
+     // WhatsApp https://www.youtube.com/watch?v=lWnqTGWlBn4 via   http://www.listentoyoutube.com/
   FileInputStream fi = new FileInputStream(a[0]);
   int b;
   while((b=fi.read())>=0) {
    if(b>127)b=255-b;
-   b=b/4;
-   for (int i=0;i<64+b;i++) {
+   b=b/2;
+   for (int i=0;i<128+b;i++) {
     System.out.print(".");
    }
    fi.read();
