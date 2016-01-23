10a4ddf4322c3aaf6666cbec7ffd0a0589705f9b
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-01-23 14:31:30 +0100

    ~SpaceDrive.java

--- SpaceDrive.java
+++ SpaceDrive.java
@@ -5,6 +5,9 @@ public class SpaceDrive {
  public static void dir(String d) throws Exception {
   Hashtable h=new Hashtable();
   for (File f:new File(d).listFiles()) {
+   if (f.getName().length()<65){
+    System.err.println(f.getName());
+   }
    String ext=f.getName().substring(65);
    if (h.containsKey(ext)) {
        h.put(ext,(Integer)h.get(ext)+1);
