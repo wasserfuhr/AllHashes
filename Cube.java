faf9526445ccc7d0ce48b182bedb9b7bd617bb8a
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-02 23:22:48 +0100

    ~Cube.java

--- Cube.java
+++ Cube.java
@@ -1,7 +1,8 @@
 public class Cube {
  public static void main(String[] a) {
   byte b=new Byte(a[0]);
-  byte b2=(byte)(1<<b);
+  int b2=1<<b;
+  System.out.println(b2);
   byte c[][][]=new byte[b2][b2][b2];
   for (byte i=0;i<b2;i++) {
    c[i][i][i]=i;
