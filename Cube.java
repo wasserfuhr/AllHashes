da9e2038c36e90e96d874596c542fdcbc9812b6c
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-02 21:45:11 +0100

    ~Cube.java

--- Cube.java
+++ Cube.java
@@ -1,6 +1,6 @@
 public class Cube {
  public static void main(String[] a) {
-  int c[][][]=new int[256][256][256];
+  int c[][][]=new int[1024][1024][1024];
   for (int i=0;i<256;i++) {
    c[i][i][i]=i;
   }
