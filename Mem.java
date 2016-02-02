388f0c1eb5375acd5e3b3a9bc2c3be176ed2e58d
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-02 20:50:00 +0100

    +Mem.java

---
+++ Mem.java
@@ -0,0 +1,7 @@
+public class Mem {
+ public static void main(String[] a) {
+  System.out.println(Runtime.getRuntime().totalMemory()+" total");
+  //	 <%=Runtime.getRuntime().freeMemory()%> free<br/>
+  // <%=Runtime.getRuntime().maxMemory()%> max<br/>
+ }
+}
