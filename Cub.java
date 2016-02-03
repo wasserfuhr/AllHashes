0641e31b5ddf07a94631b4f09fead8820850b341
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-03 02:20:52 +0100

    ~Cub.java

--- Cub.java
+++ Cub.java
@@ -1,11 +1,11 @@
-public class Cube {
+public class Cub {
  public static void main(String[] a) {
   byte b=new Byte(a[0]);
   short b2=(short)(1<<b);
   System.out.println(b2);
   byte c[][][]=new byte[b2][b2][b2];
   for (short i=0;i<b2;i++) {
-      c[i][i][i]=(byte)i;
+   c[i][i][i]=(byte)i;
   }
  }
 }
