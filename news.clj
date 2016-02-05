f52cfe27bbce2b855c92a3f7028d09dd91edf0c2
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-05 14:37:49 +0100

    ~news.clj

--- news.clj
+++ news.clj
@@ -1,3 +1,7 @@
 ; ~/Desktop/apache-maven-3.2.5/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get -Dartifact=org.clojure:clojure:1.8.0
-; java -jar ~/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar news.clj
-(println (slurp "http://www.tagesschau.de/newsticker.rdf"))
+; ~/Desktop/apache-maven-3.2.5/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:get -Dartifact=org.clojure::data.xml:0.0.8
+; java -jar ~/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar -cp ~/.m2/repository/org/clojure/data.xml/0.0.8/data.xml-0.0.8.jar:. news.clj
+(require '[clojure.xml :as xml]
+         '[clojure.zip :as zip])
+(print (xml/parse "http://www.tagesschau.de/newsticker.rdf"))
+;(clojure.data.xml/parse "http://www.tagesschau.de/newsticker.rdf")
