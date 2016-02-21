bfd01aefa48c0bc602c7d6bacb9b085101bdf9d4
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-21 18:25:53 +0100

    ~SD.java

--- SD.java
+++ SD.java
@@ -1,20 +1,21 @@
 import java.io.*;
+import java.nio.file.*;
+public class SD{
+ public static String SD() throws Exception{
+  if (System.getProperty("user.home").equals("/Users/rawa")){
+   return "/Volumes/SPACEDRIVE1/SpaceDrive/";}
+  return "/home/rawa/SpaceDrive/";}
+ public static String ext(String hash) throws Exception{
+  LineNumberReader lnr=new LineNumberReader(new InputStreamReader(new FileInputStream("FileExtLd4294.txt")));
+  String s;while((s=lnr.readLine())!=null){
+   if(s.substring(0,64).equals(hash)){
+    return s.substring(65);}}
+  //
+  DirectoryStream<Path> stream=Files.newDirectoryStream(
+   FileSystems.getDefault().getPath("H:/SpaceDrive"),"1220"+hash+".*");
+  for(final Path p:stream){
+   return .toFile().getName();}
+  return null;}
+ public static void main(String[] a) throws Exception {
+  ext("a0fcb8c6406166fcc446b455b4df9558ce441aa26536a469be4c26d784d74bd4x");}}
 
-public class SD {
- public static String SD() throws Exception {
-  if (System.getProperty("user.home").equals("/Users/rawa")) {
-   return "/Volumes/SPACEDRIVE1/SpaceDrive/";
-  }
-  return "/home/rawa/SpaceDrive/";
- }
- public static String ext(String hash) throws Exception {
-  LineNumberReader lnr = new LineNumberReader(new InputStreamReader(new FileInputStream("TmpFileExt.txt")));
-  String s;
-  while((s=lnr.readLine())!=null) {
-   if(s.substring(0,64).equals(hash)) {
-    return s.substring(65);
-   }
-  }
-  return null;
- }
-}
