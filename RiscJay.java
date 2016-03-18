8aa17ddfb0a0650f5552a7517eb84f5902fc0c66
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-18 15:01:18 +0100

    ~RiscJay.java

--- RiscJay.java
+++ RiscJay.java
@@ -1,1 +1,11 @@
-public class RiscJay{}
+import java.io.*;
+public class RiscJay{
+ public static void main(String[]a) throws Exception{
+  InputStreamReader i=new InputStreamReader(System.in);
+  int b;
+  long pc=0;
+  while((b=i.read())>=0){
+   long w=b<<8+i.read();
+   w+=w<<8+i.read();
+   w+=w<<8+i.read();
+   w+=w<<8+i.read();}}}
