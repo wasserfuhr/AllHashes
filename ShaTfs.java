6280e6317f07f382d5d55a0fc1a54c87affbd3b7
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-18 23:41:03 +0100

    ~ShaTfs.java

--- ShaTfs.java
+++ ShaTfs.java
@@ -1,9 +1,10 @@
-package com.sci.skristminer.util;
+//https://github.com/sci4me/SKristMiner/blob/master/src/main/java/com/sci/skristminer/util/SHA256.java
+//package com.sci.skristminer.util;
 
 /**
  * @author sci4me
  */
-public final class SHA256
+public final class ShaTfs
 {
     private static final int[] K = {
             0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
@@ -15,7 +16,7 @@ public final class SHA256
             0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
             0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
-    private SHA256()
+    private ShaTfs()
     {
     }
 
