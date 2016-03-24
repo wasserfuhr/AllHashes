70a9f4ac0c241fece6218a40c3c88e808b3534f2
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-24 23:38:58 +0100

    ~BfHash.c

--- BfHash.c
+++ BfHash.c
@@ -2,31 +2,24 @@
 #include <stdint.h>
 #include <time.h>
 #include <openssl/sha.h>
-
 //compile: cc -std=c99 -lcrypto BfHash.c
 // results: https://code.google.com/p/ungit/wiki/BfHash
 //to TerraDrive: e.g. echo -en "\x03\xa1\x54\xa9" |sha1sum
-
 //SeeAlso http://nayuki.eigenstate.org/res/fast-sha1-hash-implementation-in-x86-assembly/sha1-fast.c
-
 unsigned char mhash[SHA256_DIGEST_LENGTH];
-
 size_t length = 6; //bug?: 6== sizeof(data);
-
 int main(void){
-  unsigned char data[]="123456";
-
-  fprintf(stdout,"Starting BruteForce ZeroHash search for %d bytes...\n", length);
-  fflush(stdout);
-
-  memset(mhash,0xff,SHA256_DIGEST_LENGTH*2);
-  unsigned char l=0;
-  do{
+ unsigned char data[]="123456";
+ fprintf(stdout,"Starting BruteForce ZeroHash search for %d bytes...\n", length);
+ fflush(stdout);
+ memset(mhash,0xff,SHA256_DIGEST_LENGTH*2);
+ unsigned char l=0;
+ do{
    unsigned char k=0;
    do{
     unsigned char j=0x5c;
     do{
-     fprintf(stdout,"%-40s ______%02x%02x%02x\n", "", j, k,l);
+     fprintf(stdout,"%-40s ______%02x%02x%02x %ju\n","",j,k,l,(uintmax_t)time(NULL));
      fflush(stdout);
      unsigned char i=0;
      do{
@@ -42,21 +35,18 @@ int main(void){
     k++;
    }while(k!=0);
    l++;
-  }while(l!=0);
-  //printf("%ld",clock());
+ }while(l!=0);
 }
 
 int fastIter(unsigned char data[]){
-  unsigned char hash[SHA256_DIGEST_LENGTH];
-  char buf[SHA_DIGEST_LENGTH*2];
-
-  memset(buf, 0x0, SHA256_DIGEST_LENGTH*2);
-
-  unsigned char k=0;
-  do {
-   data[0]=k;
+ unsigned char hash[SHA256_DIGEST_LENGTH];
+ char buf[SHA_DIGEST_LENGTH*2];
+ memset(buf, 0x0, SHA256_DIGEST_LENGTH*2);
+ unsigned char k=0;
+ do{
+  data[0]=k;
    unsigned char l=0;
-   do {
+   do{
     data[1]=l;
     SHA256((const unsigned char *)data, length, hash);
 
@@ -75,9 +65,9 @@ int fastIter(unsigned char data[]){
       break;
      }
      j++;
-    } while(j < SHA256_DIGEST_LENGTH);
+    }while(j<SHA256_DIGEST_LENGTH);
     l++;
-   } while(l!=0);
-   k++;
-  } while (k!=0);
+  }while(l!=0);
+  k++;
+ }while(k!=0);
 }
