945ec3d694e6589bae856845da737f1b85e562ad
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-28 12:13:58 +0200

    ~LadyCatch.sh

--- LadyCatch.sh
+++ LadyCatch.sh
@@ -1,5 +1,5 @@
 b=http://webcam.frauenkirche.de/webcams/img.php
-ua=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4\"
+ua="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.4.4 (KHTML, like Gecko) Version/9.0.3 Safari/601.4.4"
 while[ 1 ]do
  date;a=`curl https://floatingboat2013.appspot.com/at`
  curl -A "$ua" $b\?12 > ~/Desktop/FrauenCam12At$a.jpg
