943f4ecd163b92f8893edadc6a06895e60493a13
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-28 12:01:06 +0200

    +LadyCatch.sh

---
+++ LadyCatch.sh
@@ -0,0 +1,6 @@
+date;a=`curl https://floatingboat2013.appspot.com/at`
+curl http://webcam.frauenkirche.de/webcams/img.php\?12 > ~/Desktop/FrauenCam12At$a.jpg
+curl http://webcam.frauenkirche.de/webcams/img.php\?22 > ~/Desktop/FrauenCam22At$a.jpg
+curl http://webcam.frauenkirche.de/webcams/img.php\?22 > ~/Desktop/FrauenCam22At$a.jpg
+sleep 25
+ 
