a9be1f8adb9a31fe2e1fdd711065e9100978e4a4
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-03-29 03:02:15 +0200

    ~YourSuperCoderClass.java

--- YourSuperCoderClass.java
+++ YourSuperCoderClass.java
@@ -1,1 +1,8 @@
-public class erCoderChallenge
+public class YourSuperCoderClass{System.out.print("1980:04-06 28-09\n1981:03-29 27-09
+1982:03-28 26-09
+1983:03-27 25-09
+1984:03-25 30-09
+1985:03-31 29-09
+1986:03-30 28-09
+1987:03-29 27-09
+1988:03-27 25-09");}
