57339ebf22a4399311f83bfdf01c3efbc4fee3a6
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-03 22:43:32 +0200

    ~Ur.java

--- Ur.java
+++ Ur.java
@@ -1,5 +1,8 @@
-public class Think {
- public static void main(String[] a) {
-  Cub.main(a);
- }
-}
+public class Ur{
+ public static void main(String[] a){
+  long lastSec=0;
+  while(true){
+   long c=System.currentTimeMillis();
+   if(c/1000>lastSec){
+    System.out.println(c);
+    lastSec=c/1000}}}
