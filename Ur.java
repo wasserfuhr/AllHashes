2f0bbb4a605d434cf3064823992ba1f045d61223
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 21:37:57 +0200

    ~Ur.java

--- Ur.java
+++ Ur.java
@@ -1,14 +1,16 @@
 public class Ur extends Thread{
  public void run(){//throws Exception{
-  long lastSec=0;
-  while(true){
-   long c=System.currentTimeMillis();
-   long t=(lastSec+1)*1000-c;
-   if(t>0){
-    sleep(t/2-10);
-   }else{
-    System.out.println(c);
-    Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
-    lastSec=c/1000;}}}
+  try{
+   long lastSec=0;
+   while(true){
+    long c=System.currentTimeMillis();
+    long t=(lastSec+1)*1000-c;
+    if(t>0){
+     sleep(t/2-10);
+    }else{
+     System.out.println(c);
+     Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
+     lastSec=c/1000;}}
+  }finally{}}
  public static void main(String[] a)throws Exception{
   new Ur().start();}}
