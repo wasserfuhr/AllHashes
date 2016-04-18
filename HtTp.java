cdc31eae5fa78d42e1163c0cfbbeb3af5883708d
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 17:34:43 +0200

    ~HtTp.java

--- HtTp.java
+++ HtTp.java
@@ -7,14 +7,15 @@ import clojure.lang.*;
 //http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
 public class HtTp extends AbstractHandler{
  public void handle(String t, Request br, HttpServletRequest rq, HttpServletResponse rs) throws IOException,ServletException{
-  RT.loadResourceScript("hiccup/core.clj");
-  RT.loadResourceScript("clojure/data/json.clj");
-  String c="(fn[rq rs]((eval(read-string(slurp \"https://dresdenlabs.appspot.com/boot\")))rq rs))";
-  PushbackReader pr = new PushbackReader(new StringReader(c));
-  Object rootHandlerExpr=LispReader.read( pr, true, null, false);
-  IFn rootHandlerFn=(IFn) Compiler.eval( rootHandlerExpr);
-  rootHandlerFn.invoke(rq,rs);
-       } finally { }%>
+  try{
+   RT.loadResourceScript("hiccup/core.clj");
+   RT.loadResourceScript("clojure/data/json.clj");
+   String c="(fn[rq rs]((eval(read-string(slurp \"https://dresdenlabs.appspot.com/boot\")))rq rs))";
+   PushbackReader pr=new PushbackReader(new StringReader(c));
+   Object rootHandlerExpr=LispReader.read(pr,true,null,false);
+   IFn rootHandlerFn=(IFn) Compiler.eval(rootHandlerExpr);
+   rootHandlerFn.invoke(rq,rs);
+  } finally { }
   br.setHandled(true);}
  public static void main(String[] a)throws Exception{
   Server s = new Server(8028);
