62fa18a37235e70c4e903af1f913282feabad48d
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 17:31:46 +0200

    ~HtTp.java

--- HtTp.java
+++ HtTp.java
@@ -3,12 +3,18 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.*;
+import clojure.lang.*;
 //http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
 public class HtTp extends AbstractHandler{
  public void handle(String t, Request br, HttpServletRequest rq, HttpServletResponse rs) throws IOException,ServletException{
-  rs.setContentType("text/html; charset=utf-8");
-  rs.setStatus(HttpServletResponse.SC_OK);
-  rs.getWriter().println("<h1>Hello World</h1>");
+  RT.loadResourceScript("hiccup/core.clj");
+  RT.loadResourceScript("clojure/data/json.clj");
+  String c="(fn[rq rs]((eval(read-string(slurp \"https://dresdenlabs.appspot.com/boot\")))rq rs))";
+  PushbackReader pr = new PushbackReader(new StringReader(c));
+  Object rootHandlerExpr=LispReader.read( pr, true, null, false);
+  IFn rootHandlerFn=(IFn) Compiler.eval( rootHandlerExpr);
+  rootHandlerFn.invoke(rq,rs);
+       } finally { }%>
   br.setHandled(true);}
  public static void main(String[] a)throws Exception{
   Server s = new Server(8028);
