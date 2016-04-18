641fa0309abea669c966476d83b88f7261cc34c9
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 23:46:03 +0200

    ~Ur.java

--- HtTp.java
+++ HtTp.java
@@ -6,26 +6,20 @@ import org.eclipse.jetty.server.handler.*;
 import clojure.lang.*;
 //http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
 public class HtTp extends AbstractHandler{
- public void handle(String t, Request br, HttpServletRequest rq, HttpServletResponse rs) throws IOException,ServletException{
+ public void handle(String t,Request br,HttpServletRequest rq,HttpServletResponse rs)throws IOException,ServletException{
   try{
    RT.loadResourceScript("hiccup/core.clj");
    //RT.loadResourceScript("clojure/data/json.clj");
    String c="(fn[rq rs]((eval(read-string(slurp\"https://knot-net.appspot.com/jboot\")))rq rs))";
    PushbackReader pr=new PushbackReader(new StringReader(c));
-   Object rootHandlerExpr=LispReader.read(pr,true,null,false);
-   IFn rootHandlerFn=(IFn)clojure.lang.Compiler.eval(rootHandlerExpr);
-   rs.getWriter().print(rootHandlerFn.invoke(rq,rs));
-  } finally { }
+   Object r=LispReader.read(pr,true,null,false);
+   IFn rf=(IFn)clojure.lang.Compiler.eval(r);
+   rs.getWriter().print(rf.invoke(rq,rs));
+  }finally{}
   br.setHandled(true);}
  public static void main(String[] a)throws Exception{
+  new Ur().start();
   Server s = new Server(8080);
   s.setHandler(new HtTp());
   s.start();
-  s.join();
-  long lastSec=0;
-  while(true){
-   long c=System.currentTimeMillis();
-   if(c/1000>lastSec){
-    System.out.println(c);
-    Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
-    lastSec=c/1000;}}}}
+  s.join();}}
