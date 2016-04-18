775353d8d711a388db1d22b812bfb6b9dce9159d
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 16:06:41 +0200

    ~HtTp.java

--- HtTp.java
+++ HtTp.java
@@ -2,16 +2,17 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.server.handler.*; 
+import org.eclipse.jetty.server.handler.*;
 //http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
-public class HtTp extends AbstractHandler{ public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-  response.setContentType("text/html; charset=utf-8");
-  response.setStatus(HttpServletResponse.SC_OK);
-  response.getWriter().println("<h1>Hello World</h1>");
-  baseRequest.setHandled(true);}}
+public class HtTp extends AbstractHandler{
+ public void handle(String target, Request baseRequest, HttpServletRequest rq, HttpServletResponse rs) throws IOException,ServletException{
+  rs.setContentType("text/html; charset=utf-8");
+  rs.setStatus(HttpServletResponse.SC_OK);
+  rs.getWriter().println("<h1>Hello World</h1>");
+  baseRequest.setHandled(true);}
  public static void main(String[] a)throws Exception{
   Server server = new Server(8028);
-  server.setHandler(new HelloWorld());
+  server.setHandler(new HtTp());
   server.start();
   server.join();
   long lastSec=0;
