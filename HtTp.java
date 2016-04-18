ae2ccdcae957a6d0f2d91a43c3ac3c2a1589d454
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 16:04:43 +0200

    ~HtTp.java

--- HtTp.java
+++ HtTp.java
@@ -1,5 +1,19 @@
-public class Ur{
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.*; 
+//http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
+public class HtTp extends AbstractHandler{ public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+  response.setContentType("text/html; charset=utf-8");
+  response.setStatus(HttpServletResponse.SC_OK);
+  response.getWriter().println("<h1>Hello World</h1>");
+  baseRequest.setHandled(true);}}
  public static void main(String[] a)throws Exception{
+  Server server = new Server(8028);
+  server.setHandler(new HelloWorld());
+  server.start();
+  server.join();
   long lastSec=0;
   while(true){
    long c=System.currentTimeMillis();
@@ -7,31 +21,3 @@ public class Ur{
     System.out.println(c);
     Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
     lastSec=c/1000;}}}}
-http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
-package org.eclipse.jetty.embedded;
- 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
- 
-public class HelloWorld extends AbstractHandler {
- 
-    @Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html; charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("<h1>Hello World</h1>");
-        baseRequest.setHandled(true);
-    }
- 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        server.setHandler(new HelloWorld());
-        server.start();
-        server.join();
-    }
-}
