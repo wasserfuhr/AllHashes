0180982f9fe9a0de573e6430d1b02dd2da38a67a
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 15:58:59 +0200

    +HtTp.java

---
+++ HtTp.java
@@ -0,0 +1,9 @@
+public class Ur{
+ public static void main(String[] a)throws Exception{
+  long lastSec=0;
+  while(true){
+   long c=System.currentTimeMillis();
+   if(c/1000>lastSec){
+    System.out.println(c);
+    Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
+    lastSec=c/1000;}}}}
