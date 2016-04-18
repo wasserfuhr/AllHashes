51ebd46ca62f38bbbb647383e6aee111faae9c8a
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-18 16:00:35 +0200

    ~HtTp.java

--- HtTp.java
+++ HtTp.java
@@ -7,3 +7,31 @@ public class Ur{
     System.out.println(c);
     Runtime.getRuntime().exec("/home/rawa/git/AllHashes/job.sh");
     lastSec=c/1000;}}}}
+http://www.eclipse.org/jetty/documentation/current/advanced-embedding.html :
+package org.eclipse.jetty.embedded;
+ 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+ 
+public class HelloWorld extends AbstractHandler {
+ 
+    @Override
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Hello World</h1>");
+        baseRequest.setHandled(true);
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new HelloWorld());
+        server.start();
+        server.join();
+    }
+}
