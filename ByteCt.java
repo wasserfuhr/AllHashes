6c38e8f72d3ee97440672f18b39eb79b9267fa3e
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-28 11:07:41 +0200

    ~ByteCt.java

--- ByteCt.java
+++ ByteCt.java
@@ -1,14 +1,8 @@
 import java.io.*;
 public class ByteCt{
  public static void main(String[]a) throws Exception {
-  InputStreamReader r=new InputStreamReader(System.in));
+  InputStreamReader r=new InputStreamReader(System.in);
   int b;
   int[] sig=new int[256];
-  while((b=fr.read())>=0){
-   sig[b]++;
-  }
-  for(int i=0;i<256;i++){
-   System.out.println(b+" "+sig[i]);
-  }
- }
-}
+  while((b=fr.read())>=0)sig[b]++;
+  for(int i=0;i<256;i++)System.out.println(b+" "+sig[i]);}}
