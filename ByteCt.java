4911648f4c0939d4824d76206345490cb9916e15
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-28 11:08:01 +0200

    ~ByteCt.java

--- ByteCt.java
+++ ByteCt.java
@@ -4,5 +4,5 @@ public class ByteCt{
   InputStreamReader r=new InputStreamReader(System.in);
   int b;
   int[] sig=new int[256];
-  while((b=fr.read())>=0)sig[b]++;
+  while((b=r.read())>=0)sig[b]++;
   for(int i=0;i<256;i++)System.out.println(b+" "+sig[i]);}}
