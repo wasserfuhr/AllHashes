bd605a4390ab13bbbce48d9ac8d5490fc55c4b90
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-26 14:26:55 +0200

    ~bcDump.clj

--- bcDump.clj
+++ bcDump.clj
@@ -1,1 +1,6 @@
-(subs(slurp"http://webbtc.com/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f.hex")160)
+(let[h 0x64f37
+ a(atom h)]
+ (while(> 0x64f33 @a)
+  (do(print @a)
+   (println(subs(slurp"http://webbtc.com/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f.hex")160))
+   (swap! a dec))))
