4d967572ce1cac78142f81341f420ddcd5551f7e
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-26 14:27:25 +0200

    ~bcDump.clj

--- bcDump.clj
+++ bcDump.clj
@@ -1,6 +1,6 @@
 (let[h 0x64f37
  a(atom h)]
- (while(> 0x64f33 @a)
+ (while(> @a 0x64f33)
   (do(print @a)
    (println(subs(slurp"http://webbtc.com/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f.hex")160))
    (swap! a dec))))
