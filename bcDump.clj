f41b98034576c4c1efdfbdda6ac8ab1d519ab16b
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-26 14:18:04 +0200

    +bcDump.clj

--- bcDump.clj
+++ bcDump.clj
@@ -1,1 +1,1 @@
-(subs(slurp"https://webbtc.com/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f.hex")160)
+(subs(slurp"http://webbtc.com/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f.hex")160)
