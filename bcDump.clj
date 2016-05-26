1aac012959369fef5a7e975a4be559f7e91781ae
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-26 14:49:22 +0200

    ~bcDump.clj

--- bcDump.clj
+++ bcDump.clj
@@ -1,16 +1,11 @@
 (let[h 0x64f37 bx"054b355269379e17f460069efee72f2b739fad76a0ee29ad"
- a(atom h)h(atom 0)]
+ a(atom h)h(atom (str"0000000000000000"bx)]
  (while(> @a 0x64f33)
-  (do(println @a)(let[s(subs(slurp(str"http://webbtc.com/block/0000000000000000"bx".hex"))0 160)
-  af31be8401dd6bfb350c923aca55833b0c554990a1e695040000000000000000
-  (map 
-(let[s"af31be8401dd6bfb350c923aca55833b0c554990a1e695040000000000000000"]
- (apply str
-  (map(fn[i](str
-               (.charAt s(* 2 (- 31 i)))
-               (.charAt s(+(* 2 (- 31 i))1))
-               ))(range 32))))
-   (set! h 
-af31be8401dd6bfb350c923aca55833b0c554990a1e695040000000000000000
+  (do(println @a)(let[s(subs(slurp(str"http://webbtc.com/block/"@h".hex"))0 160)
+   (set! h(let(apply str
+    (map(fn[i](str
+      (.charAt s(* 2 (- 31 i)))
+      (.charAt s(+(* 2 (- 31 i))1))
+     ))(range 32))))
    (println(subs(slurp(str"http://webbtc.com/block/0000000000000000"bx".hex"))0 160))
    (swap! a dec))))
