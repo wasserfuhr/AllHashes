c7b80d84021a7db2a0fdaa6beee51e8e41da94ea
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-28 09:53:13 +0200

    ~l3b.jpg

--- SD.java
+++ SD.java
@@ -6,17 +6,21 @@ public class SD{
    return "/Volumes/SPACEDRIVE1/SpaceDrive/";}
   return "/home/rawa/SpaceDrive/";}
  public static String ext(String hash) throws Exception{
-  LineNumberReader lnr=new LineNumberReader(new InputStreamReader(new FileInputStream("FileExtLd4294.txt")));
+  FileInputStream f=new FileInputStream("FileExtLd4294.txt");
+  InputStreamReader i=new InputStreamReader(f);
+  LineNumberReader lnr=new LineNumberReader(i);
   String s;while((s=lnr.readLine())!=null){
    if(s.substring(0,64).equals(hash)){
     return s.substring(65);}}
   DirectoryStream<Path>stream=Files.newDirectoryStream(
    FileSystems.getDefault().getPath(SD()),"1220"+hash+".*");
   for(final Path p:stream){
-   File f=p.toFile();
-   String s=f.getName().substring(69);
+   String r=p.toFile().getName().substring(69);
+   stream.close();
    f.close();
-   return s;}
+   return r;}
+  stream.close();
+  f.close();
   return null;}
  public static void main(String[]a)throws Exception{
   ext("a0fcb8c6406166fcc446b455b4df9558ce441aa26536a469be4c26d784d74bd4x");}}
