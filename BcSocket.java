b638d2a1929d870f2e51418f6db75986743ebee1
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-30 13:13:18 +0200

    ~BcSocket.java

--- BcSocket.java
+++ BcSocket.java
@@ -1,37 +1,21 @@
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 public class BcSocket extends WebSocketClient{
 
     @Override
 	public void onMessage( String message ) {
-	ta.append( "got: " + message + "\n" );
-	ta.setCaretPosition( ta.getDocument().getLength() );
     }
 
     @Override
 	public void onOpen( ServerHandshake handshake ) {
-	ta.append( "You are connected to ChatServer: " + getURI() + "\n" );
-	ta.setCaretPosition( ta.getDocument().getLength() );
     }
 
     @Override
 	public void onClose( int code, String reason, boolean remote ) {
-	ta.append( "You have been disconnected from: " + getURI() + "; Code: " + code + " " + reason + "\n" );
-	ta.setCaretPosition( ta.getDocument().getLength() );
-	connect.setEnabled( true );
-	uriField.setEditable( true );
-	draft.setEditable( true );
-	close.setEnabled( false );
     }
 
     @Override
 	public void onError( Exception ex ) {
-	ta.append( "Exception occured ...\n" + ex + "\n" );
-	ta.setCaretPosition( ta.getDocument().getLength() );
-	ex.printStackTrace();
-	connect.setEnabled( true );
-	uriField.setEditable( true );
-	draft.setEditable( true );
-	close.setEnabled( false );
     }
  public static void main(String[] a){
   new BcSocket(new java.net.URI("wss://ws.blockchain.info/inv"));}}
