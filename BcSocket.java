1de6bc289ba5a1188d4f830c9408ffa473ba114e
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-30 13:09:14 +0200

    ~BcSocket.java

--- BcSocket.java
+++ BcSocket.java
@@ -1,4 +1,4 @@
-import org.java_websocket.client;
+import org.java_websocket.client.WebSocketClient;
 public class BcSocket extends WebSocketClient{
  public static void main(String[] a){
   new BcSocket(new java.net.URI("wss://ws.blockchain.info/inv"));}}
