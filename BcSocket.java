bf73d4bd64e325468447d2446e35154f9c4894d8
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-30 13:50:15 +0200

    ~BcSocket.java

--- BcSocket.java
+++ BcSocket.java
@@ -3,7 +3,7 @@ import org.java_websocket.handshake.ServerHandshake;
 public class BcSocket{
  public static void main(String[] a) throws Exception{
   org.java_websocket.WebSocketImpl.DEBUG = true;
-  WebSocketClient c=new WebSocketClient(new java.net.URI("wss://ws.blockchain.info/inv")){
+  WebSocketClient c=new WebSocketClient(new java.net.URI("ws://ws.blockchain.info/inv")){
    @Override public void onMessage(String message ){System.out.println(message);}
    @Override public void onOpen(ServerHandshake handshake){System.out.println("open!");}
    @Override public void onClose(int code,String reason,boolean remote){}
