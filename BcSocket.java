da4c44ac34a58caad46ea675dd8cad62ef103cc3
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-05-30 13:23:52 +0200

    ~BcSocket.java

--- BcSocket.java
+++ BcSocket.java
@@ -1,21 +1,13 @@
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 public class BcSocket extends WebSocketClient{
-
+ public static void main(String[] a) throws Exception{
+  WebSocketClient c=new WebSocketClient(new java.net.URI("wss://ws.blockchain.info/inv")){
+   @Override
+   public void onMessage(String message ){}
+   @Override
+   public void onOpen( ServerHandshake handshake ) {}
     @Override
-	public void onMessage( String message ) {
-    }
-
+	public void onClose( int code, String reason, boolean remote ) {}
     @Override
-	public void onOpen( ServerHandshake handshake ) {
-    }
-
-    @Override
-	public void onClose( int code, String reason, boolean remote ) {
-    }
-
-    @Override
-	public void onError( Exception ex ) {
-    }
- public static void main(String[] a){
-  new BcSocket(new java.net.URI("wss://ws.blockchain.info/inv"));}}
+	public void onError( Exception ex ) {}};}}
