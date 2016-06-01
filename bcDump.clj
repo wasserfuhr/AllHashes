d3ebad40839a9a98e6fbd306dc4053a590156bd9
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-06-01 12:18:00 +0200

    ~bcDump.clj +bh6525a.txt

--- bcDump.clj
+++ bcDump.clj
@@ -4,9 +4,11 @@
 ;0x5e469 bx"0936c69fb727394141787c32ce6a16e2a9e6a328b2401fed" ;502 Bad Gateway
 ;0x65049 bx"045d9003d09f64af5a263fd21ea691d5d6dc19907e594e31"
 ;0x65157 bx"0378aa86c50d9b4270b7cad11e13ded6b21aa0171708961c"
-0x65107 bx"04e1f222edf916d37fd6f322cf3e96db87b1e832005c834a"
+;0x65107 bx"04e1f222edf916d37fd6f322cf3e96db87b1e832005c834a"
+0x6525a bx"02b486b6ab1364c21a7b15681fcdc927a6934a0b322d4961"
  a(atom h)n(atom (str"0000000000000000"bx))]
- (while(> @a 0x61e25)
+ (while(> @a 0x65157)
+; (while(> @a 0x61e25)
   (let[ah(format"%x"@a)
     s0(slurp(str"http://webbtc.com/block/"@n".hex"))
     sl(subs s0 0 160)
@@ -21,4 +23,4 @@
  (.println *err* ah) 
     (println ah @n)
     (swap! a dec)
-    (Thread/sleep 4000)))))
+    (Thread/sleep 2000)))))
