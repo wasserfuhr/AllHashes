37b8f4dd16d38eeb20a170bdf2779957e7dfd675
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-06-02 17:31:03 +0200

    ~MyWebSocketClient.java

--- MyWebSocketClient.java
+++ MyWebSocketClient.java
@@ -1,18 +1,15 @@
 //http://stackoverflow.com/questions/8352980/application-to-application-communication-through-web-sockets
 import org.eclipse.jetty.websocket.*;
 public class MyWebSocketClient{
-  MyWebSocketClient() throws IOException
-    {       
-        WebSocketClientFactory factory = new WebSocketClientFactory();
-        try {
-          factory.start();
-        } catch(Exception e){e.printStackTrace();}
-        WebSocketClient client = factory.newWebSocketClient();
-        WebSocket.Connection connection = client.open(new URI("ws://myserver:8080/testws"),new WebSocket.OnTextMessage(){
-		public void onOpen(Connection connection){}
-		public void onClose(int closeCode, String message){}
-
-	   public void onMessage(String data){}
-	    }).get(50, TimeUnit.SECONDS);
-
-	    }
+ MyWebSocketClient()throws IOException{       
+  WebSocketClientFactory factory=new WebSocketClientFactory();
+  try{
+   factory.start();
+  }catch(Exception e){e.printStackTrace();}
+  WebSocketClient client = factory.newWebSocketClient();
+  WebSocket.Connection connection = client.open(new URI("ws://myserver:8080/testws"),new WebSocket.OnTextMessage(){
+   public void onOpen(Connection connection){}
+   public void onClose(int closeCode, String message){}
+   public void onMessage(String data){}
+  }).get(50, TimeUnit.SECONDS);
+ }}
