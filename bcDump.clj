a973a0b0afe59e6fd7ef75026ffd0db51562a76d
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-06-03 16:45:33 +0200

    ~bcDump.clj

--- bcDump.clj
+++ bcDump.clj
@@ -7,9 +7,13 @@
 ;0x65107 bx"04e1f222edf916d37fd6f322cf3e96db87b1e832005c834a"
 ;0x6525a bx"02b486b6ab1364c21a7b15681fcdc927a6934a0b322d4961"
 ;0x64847 bx"043b58e1e6a801ef7951d2e92727fdfc5caede6c36e7ffc3"
-0x63f1a bx"00ef26364ac2cd96d688bca642c2639d480955f855d45743"
+;0x63f1a bx"00ef26364ac2cd96d688bca642c2639d480955f855d45743"
+;0x631af bx"03b0f4768c4f2000e6bcefb63db565c1d0dd98cffd4090f6"
+;0x6538d bx"01d36823a26028a49a045293e305f3c961ffa932923d086b"
+0x652be bx"039945e71f68b114976022bf41477e87ffe9e287bcb3fb89"
  a(atom h)n(atom (str"0000000000000000"bx))]
- (while(> @a 0x61e25)
+ (while(> @a 0x65259)
+; (while(> @a 0x65157)
 ; (while(> @a 0x65157)
   (let[ah(format"%x"@a)
     s0(slurp(str"http://webbtc.com/block/"@n".hex"))
@@ -25,4 +29,4 @@
  (.println *err* ah) 
     (println ah @n)
     (swap! a dec)
-    (Thread/sleep 2000)))))
+    (Thread/sleep 1000)))))
