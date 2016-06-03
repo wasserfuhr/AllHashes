c267b083c66785d57f804dd913587d7c2271aa04
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-06-03 10:32:56 +0200

    ~MyWebSocketClient.java

--- MyWebSocketClient.java
+++ MyWebSocketClient.java
@@ -3,15 +3,22 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
 import org.eclipse.jetty.websocket.*;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
 public class MyWebSocketClient{
  MyWebSocketClient()throws Exception{
   WebSocketClientFactory factory=new WebSocketClientFactory();
   try{
    factory.start();
   }catch(Exception e){e.printStackTrace();}
-  WebSocketClient client = factory.newWebSocketClient();
+  //  Configuration config = new Configuration();
+  WebSocketClient client //= factory.newWebSocketClient();
+      //  = new WebSocketClient(new SslContextFactory(config.unsafeSsl()));
+  = new WebSocketClient(new SslContextFactory(true));
+
+      
   WebSocket.Connection connection = client.open(new URI("ws://ws.blockchain.info/inv"),new WebSocket.OnTextMessage(){
-   public void onOpen(Connection connection){}
+   public void onOpen(Connection connection){System.out.println("open?");}
    public void onClose(int closeCode, String message){}
    public void onMessage(String data){}
   }).get(50, TimeUnit.SECONDS);
