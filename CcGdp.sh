50272d598574d35277de7a11cf8d1768a0e503b6
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-09-06 20:24:40 +0200

    +CcGdp.sh

---
+++ CcGdp.sh
@@ -0,0 +1,8 @@
+curl https://floatingboat2013.appspot.com/gdp?q=Q228
+curl https://floatingboat2013.appspot.com/gdp?q=Q878
+curl https://floatingboat2013.appspot.com/gdp?q=Q889
+curl https://floatingboat2013.appspot.com/gdp?q=Q781
+curl https://floatingboat2013.appspot.com/gdp?q=Q222
+curl https://floatingboat2013.appspot.com/gdp?q=Q399
+curl https://floatingboat2013.appspot.com/gdp?q=Q916
+curl https://floatingboat2013.appspot.com/gdp?q=Q414
