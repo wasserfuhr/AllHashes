82ac252430af25df91c753d0c7dc20bac7f7cb33
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-09-06 20:54:36 +0200

    ~CcEn.sh

--- CcEn.sh
+++ CcEn.sh
@@ -1,4 +1,4 @@
-echo -n ad;curl https://floatingboat2013.appspot.com/CcEn?q=Q228
-echo -n ae;curl https://floatingboat2013.appspot.com/CcEn?q=Q878
-echo -n af;curl https://floatingboat2013.appspot.com/CcEn?q=Q889
-echo -n ag;curl https://floatingboat2013.appspot.com/CcEn?q=Q781
+echo -n ad ;curl https://floatingboat2013.appspot.com/CcEn?q=Q228;echo
+echo -n ae ;curl https://floatingboat2013.appspot.com/CcEn?q=Q878;echo
+echo -n af ;curl https://floatingboat2013.appspot.com/CcEn?q=Q889;echo
+echo -n ag ;curl https://floatingboat2013.appspot.com/CcEn?q=Q781;echo
