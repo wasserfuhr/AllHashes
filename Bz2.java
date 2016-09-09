b85f62577df1b62ec43a9c224f6a7f3826d611aa
RainerWasserfuhr <wasserfuhr@yahoo.com>
2016-09-09 10:44:52 +0200

    Update Bz2.java

--- Bz2.java
+++ Bz2.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.nio.charset.Charset;
 import org.apache.tools.bzip2.CBZip2InputStream;
 
-public class Bz2 {
- public static void main(String[] a) throws Exception {
+public class Bz2{
+ public static void main(String[] a) throws Exception{
   //http://www.programcreek.com/java-api-examples/index.php?api=org.apache.tools.bzip2.CBZip2InputStream
   InputStream is = new BufferedInputStream(new FileInputStream(a[0]), 8192);
   is.read(); is.read();
   is = new CBZip2InputStream(is);
   LineNumberReader l = new LineNumberReader(new InputStreamReader(is, Charset.forName("UTF-8")));
   String s;
-  while((s=l.readLine())!=null) {
+  while((s=l.readLine())!=null){
    System.out.println(s);
   }
  }
