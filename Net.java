f99ee911669fae4306da02600f05610a92e7dd76
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-11-03 20:42:49 +0100

    ~*

--- Net.java
+++ Net.java
@@ -1,17 +1,16 @@
 import java.net.*;
 //https://systembash.com/a-simple-java-udp-server-and-udp-client/
 public class Net{
- public static void main(String[] a)throws Exception{
-  DatagramSocket s=new DatagramSocket(9876);
+ public static void main(String[]a)throws Exception{
+  DatagramSocket s=new DatagramSocket(1337);
   byte[] rd=new byte[1024];
   byte[] sd=new byte[1024];
   while(true){
-   DatagramPacket rp=new DatagramPacket(receiveData, receiveData.length);
-   s.receive(receivePacket);
+   DatagramPacket rp=new DatagramPacket(rd,rd.length);
+   s.receive(rp);
    String sen=new String(rp.getData());
-   System.out.println("IN: " + sen);
-   System.out.println(" " + sen.getLength());
-   InetAddress IPAddress=rp.getAddress();
+   System.out.println("IN: "+sen);
+   System.out.println(" "+rp.getLength());
    sd=sen.toUpperCase().getBytes();
-   DatagramPacket sp=new DatagramPacket(sd, sd.length, IPAddress,rp.getPort());
+   DatagramPacket sp=new DatagramPacket(sd,sd.length,rp.getAddress(),rp.getPort());
    s.send(sp);}}}
