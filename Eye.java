115269f35823d04410c43361a716c2afc2bf991d
root <root@149-190-47-212.fsit.ch>
2017-08-01 21:00:25 +0200

    git add at36b49ea.64x3ce0817.SbOneHist.txt at378256e.0e.SbOneSuHist.txt p1.jpg

--- Eye.java
+++ Eye.java
@@ -1,20 +1,16 @@
-import java.io.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import javax.imageio.ImageIO;
 
-public class Eye {
- public static void main(String[] args) throws Exception {
-  File f = new File(args[0]);
+public class Eye{
+ public static void main(String[] args) throws Exception{
+  File f=new File(args[0]);
   //http://stackoverflow.com/questions/6524196/java-get-pixel-array-from-image
   BufferedImage i=ImageIO.read(f);
   System.out.println(i.getWidth()+"x"+i.getHeight());
-  for (int y=0;y<128;y++) {
-   for (int x=0;x<128;x++) {
-    int p=i.getRGB(600+x,800+y);
+  for (int y=0;y<256;y++){
+   for (int x=0;x<128;x++){
+    int p=i.getRGB(200+x,1111+y);
     String s=String.format("%02x ",p);
-    System.out.print(s.substring(2,3));
-   }
-   System.out.println();
-  }
- }
-}
+    System.out.print(s.substring(2,3));}
+   System.out.println();}}}
